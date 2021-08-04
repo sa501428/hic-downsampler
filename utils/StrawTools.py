9505a8a43dfe6ac6e67421bb8332e09c05f38338
Muhammad S Shamim <sa501428@gmail.com>
2021-08-04 06:06:55 -0500

    update utils

--- utils/StrawTools.py
+++ utils/StrawTools.py
@@ -1,6 +1,7 @@
 import numpy as np
-from scipy import sparse
 import strawC
+from scipy import sparse
+from scipy.sparse import coo_matrix
 
 
 class HiCFile:
@@ -58,3 +59,74 @@ class HiCFile:
         cy1 = y1 * resolution
         cy2 = (y1 + width - 1) * resolution
         return self.grab_intra_region(chrom1, cx1, cx2, cy1, cy2, resolution, width)
+
+    def get_chromosome_dot_sizes(hic_file: str):
+        cds = {}
+        chromosome_dot_sizes = strawC.getChromosomes(hic_file)
+        for chromosome in chromosome_dot_sizes:
+            temp_name = str(chromosome.name).lower()
+            if "all" in temp_name or "mt" in temp_name or "y" in temp_name:
+                continue
+            try:
+                chromosome_index = int(temp_name.replace("chr", ""))
+                cds[chromosome_index] = chromosome.length
+            except:
+                pass
+        return cds
+
+    def extract_all_data_hic(url, normalization, resolution, chroms1, chroms2, chrom_dot_sizes):
+        overall_array = None
+        for chr1 in chroms1:
+            current_row = None
+            for chr2 in chroms2:
+                needToFlip = False
+                achr1, achr2 = chr1, chr2
+
+                if int(chr2) < int(chr1):
+                    needToFlip = True
+                    achr1, achr2 = chr2, chr1
+
+                max_rows = int(chrom_dot_sizes[achr1] / resolution) + 1
+                max_cols = int(chrom_dot_sizes[achr2] / resolution) + 1
+
+                result = strawC.strawC("observed", normalization, url, achr1, achr2, 'BP', resolution)
+
+                resultX = list()
+                resultY = list()
+                resultVal = list()
+
+                for i in range(len(result)):
+                    resultX.append(int(result[i].binX))
+                    resultY.append(int(result[i].binY))
+                    resultVal.append(result[i].counts)
+                if int(chr2) == int(chr1):
+                    for i in range(len(result)):
+                        if int(result[i].binX) != int(result[i].binY):
+                            resultX.append(int(result[i].binY))
+                            resultY.append(int(result[i].binX))
+                            resultVal.append(result[i].counts)
+
+                row_indices = np.asarray(resultX) / resolution
+                col_indices = np.asarray(resultY) / resolution
+                data = resultVal
+                mat_coo = coo_matrix((data, (row_indices.astype(int), col_indices.astype(int))),
+                                     shape=(max_rows, max_cols))
+
+                # put data into a dense matrix format
+                dense = mat_coo.toarray()
+                dense[np.isnan(dense)] = 0  # set NaNs to zero
+
+                if needToFlip:
+                    dense = dense.T
+                print('.')
+
+                if current_row is None:
+                    current_row = dense
+                else:
+                    current_row = np.hstack((current_row, dense))
+
+            if overall_array is None:
+                overall_array = current_row
+            else:
+                overall_array = np.vstack((overall_array, current_row))
+        return overall_array
